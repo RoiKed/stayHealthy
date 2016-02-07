76d465f5c77f15382616739c6cf6685c75c05265
Roi <roiked@gmail.com>
2016-02-07 18:16:33 +0200

    added the SwiftData.swift

--- StayHeathy/MenuScreen.swift
+++ StayHeathy/MenuScreen.swift
@@ -11,16 +11,15 @@ import UIKit
 class MenuScreen: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
     var personDetails :NSDictionary!
-    var dataToPresent = [sqlCategory]()
-    let db = SQLiteDB.sharedInstance()
-
+    //var dataToPresent = [sqlCategory]()
+    //let db = SQLiteDB.sharedInstance()
     @IBOutlet weak var hartBtn: UIButton!
     @IBOutlet weak var generalBtn: UIButton!
     @IBOutlet weak var cancerBtn: UIButton!
     @IBOutlet weak var mouthBtn: UIButton!
     @IBOutlet weak var vacsinesBtn: UIButton!
-    
     @IBOutlet weak var sqlDataCollection: UICollectionView!
+    var rows:[SwiftData.SDRow] = []
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
@@ -28,8 +27,20 @@ class MenuScreen: UIViewController, UICollectionViewDelegate, UICollectionViewDa
         if (userDefaults.objectForKey("user_details") != nil) {
             self.personDetails = (userDefaults.objectForKey("user_details") as! NSDictionary)
         }
-        let constraints = self.getBasicSqlConstraints()
-        dataToPresent = sqlCategory().allRows("name ASC", constraints: constraints)
+        let basicQuery = self.getBasicSqlQury()
+        let (resultSet, err) = SwiftData.executeQuery(basicQuery)
+        self.rows = resultSet
+        
+        if err != nil {
+            //there was an error during the query, handle it here
+        } else {
+            for row in resultSet {
+                if let name = row["Name"]?.asString() {
+                    print("The Test name is: \(name)")
+                }
+            }
+        }
+        //dataToPresent = sqlCategory().allRows("name ASC", constraints: constraints)
         sqlDataCollection.reloadData()
     }
     
@@ -40,33 +51,36 @@ class MenuScreen: UIViewController, UICollectionViewDelegate, UICollectionViewDa
     
     // MARK: UICollectionViewDataSource
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return dataToPresent.count;
+        return self.rows.count      //dataToPresent.count;
     }
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         let cell:SHViewCell = collectionView.dequeueReusableCellWithReuseIdentifier("CategoryCell", forIndexPath: indexPath) as! SHViewCell
-        let collectionRow = dataToPresent[indexPath.row]
-        cell.title.text = collectionRow.name
+        //let collectionRow = dataToPresent[indexPath.row]
+        cell.title.text = self.rows
         let imageName = collectionRow.image
         cell.titleImage.image = UIImage(named: imageName);
         return cell
     }
     
-    func getBasicSqlConstraints() -> String {
+    func getBasicSqlQury() -> String {
         let age = self.personDetails .objectForKey("age") as! String
-        let basicConsteaints = " WHERE start_age < " + age //+ " AND en__age > " + age
+        let basicConsteaints = "SELECT * FROM sheet1 WHERE start_age < " + age //+ " AND en__age > " + age
         return basicConsteaints
     }
     
     func getBasicSqlConstraints(addedConstraints:String)  {
-        let fullSqlConsteaints = self.getBasicSqlConstraints() + addedConstraints
-        dataToPresent = sqlCategory().allRows("name ASC", constraints: fullSqlConsteaints)
+        let fullSqlConsteaints = self.getBasicSqlQury() + addedConstraints
+        //dataToPresent = sqlCategory().allRows("name ASC", constraints: fullSqlConsteaints)
+        
+        
         sqlDataCollection.reloadData()
     }
     
     @IBAction func hartButtonTapped(sender: UIButton) {
-        let addedConstraints = " AND movie = hart"
-        self.getBasicSqlConstraints(addedConstraints)
+        let addedConstraints = " AND parent = hart"
+        let fullSqlConsteaints = self.getBasicSqlQury() + addedConstraints
+        
         self.sqlDataCollection.backgroundColor = UIColor.purpleColor()
     }
     
